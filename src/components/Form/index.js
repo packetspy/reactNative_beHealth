b14dcd48fb632bfa71eccb9e87cf8c93450b1eeb
Anderson Tomazeto <packetspy@gmail.com>
2024-04-07 01:09:57 -0300

    Fixing: Pressable Color

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,73 +1,73 @@
-import React, { useState } from 'react'
-import {
-    Keyboard,
-    Pressable,
-    Text,
-    TextInput,
-    View
-} from 'react-native'
-import ResultImc from './ResultImc'
-import styles from './style'
+import React, { useState } from "react";
+import { Keyboard, Pressable, Text, TextInput, View } from "react-native";
+import ResultImc from "./ResultImc";
+import styles from "./style";
 
 const Form = () => {
-    const [height, setHeight] = useState('')
-    const [weight, setWeight] = useState('')
-    const [message, setMessage] = useState('Preencha peso e altura')
-    const [imc, setImc] = useState(null)
-    const [textButton, setTextButton] = useState('Calcular')
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [message, setMessage] = useState("Preencha peso e altura");
+  const [imc, setImc] = useState(null);
+  const [textButton, setTextButton] = useState("Calcular IMC");
 
-    const imcCalculator = () => setImc((weight / (height * height)).toFixed(2))
+  const imcCalculator = () => setImc((weight / (height * height)).toFixed(2));
+  const [timesPressed, setTimesPressed] = useState(0);
 
-    const imcValidator = () => {
-        console.log('imc Validator')
+  const imcValidator = () => {
+    setTimesPressed((current) => current + 1);
+    console.log(`imc Validator ${timesPressed}`);
 
-        if (weight != '' && height != '') {
-            imcCalculator()
-            setHeight('')
-            setWeight('')
-            setMessage('Seu IMC é de:')
-            setTextButton('Calcular novamente')
-            return
-        }
-        setTextButton('Calcular IMC')
-        setMessage('Preencha peso e altura')
-        setImc(null)
+    if (weight != "" && height != "") {
+      imcCalculator();
+      setHeight("");
+      setWeight("");
+      setMessage("Seu IMC é de:");
+      setTextButton("Calcular novamente");
+      return;
     }
+    setTextButton("Calcular IMC");
+    setMessage("Preencha peso e altura");
+    setImc(null);
+  };
 
-    return (
-        <View style={styles.formContext}>
-            <View style={styles.form}>
-                <Text>Altura</Text>
-                <TextInput
-                    onChangeText={setHeight}
-                    value={height}
-                    placeholder="Ex: 1.70"
-                    inputMode="numeric"
-                    style={styles.formField}
-                />
-                <Text>Peso</Text>
-                <TextInput
-                    onChangeText={setWeight}
-                    value={weight}
-                    placeholder="Ex: 70.32"
-                    inputMode="numeric"
-                    style={styles.formField}
-                />
+  return (
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text>Altura</Text>
+        <TextInput
+          onChangeText={setHeight}
+          value={height}
+          placeholder="Ex: 1.70"
+          inputMode="numeric"
+          style={styles.formField}
+        />
+        <Text>Peso</Text>
+        <TextInput
+          onChangeText={setWeight}
+          value={weight}
+          placeholder="Ex: 70.32"
+          inputMode="numeric"
+          style={styles.formField}
+        />
 
-                <Pressable onPress={() => imcValidator()} onPressOut={() => { Keyboard.dismiss() }}
-                    style={({ pressed }) => [
-                        {
-                            backgroundColor: pressed ? styles.buttonCalculatePressed : styles.buttonCalculate,
-                        },
-                        styles.buttonCalculate,
-                    ]}>
+        <Pressable
+          onPress={() => {
+            imcValidator();
+          }}
+          onPressOut={() => {
+            Keyboard.dismiss();
+          }}
+          style={({ pressed }) => [
+            { backgroundColor: pressed ? "#ff004350" : "#ff0043" },
+            styles.buttonDefault,
+          ]}
+        >
+          <Text style={styles.buttonText}>{textButton}</Text>
+        </Pressable>
+      </View>
+      <ResultImc message={message} imc={imc} />
+    </View>
+  );
+};
 
-                    <Text style={styles.buttonText}>{textButton}</Text>
-                </Pressable>
-            </View>
-            <ResultImc message={message} imc={imc} />
-        </View >
-    )
-}
-
-export default Form
+export default Form;
--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -1,69 +1,55 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    formContext: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#fff',
-        bottom: 0,
-        alignItems: 'center',
-        marginTop: 30,
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30
-    },
+  formContext: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#fff",
+    bottom: 0,
+    alignItems: "center",
+    marginTop: 30,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
 
-    form: {
-        width: '100%',
-        height: 'auto',
-        marginTop: 30,
-        padding: 20,
-    },
+  form: {
+    width: "100%",
+    height: "auto",
+    marginTop: 30,
+    padding: 20,
+  },
 
-    formLabel: {
-        color: '#000',
-        fontSize: 18,
-        paddingLeft: 20
-    },
+  formLabel: {
+    color: "#000",
+    fontSize: 18,
+    paddingLeft: 20,
+  },
 
-    formField: {
-        width: '90%',
-        height: 40,
-        borderRadius: 5,
-        backgroundColor: '#f6f6f6',
-        paddingLeft: 10,
-        margin: 12,
-        fontSize: 16
-    },
+  formField: {
+    width: "90%",
+    height: 40,
+    borderRadius: 5,
+    backgroundColor: "#f6f6f6",
+    paddingLeft: 10,
+    margin: 12,
+    fontSize: 16,
+  },
 
-    buttonCalculate: {
-        width: '90%',
-        paddingTop: 14,
-        paddingBottom: 14,
-        borderRadius: 20,
-        backgroundColor: '#ff0043',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginLeft: 12,
-        margin: 30
-    },
-    buttonCalculatePressed: {
-        width: '90%',
-        paddingTop: 14,
-        paddingBottom: 14,
-        borderRadius: 20,
-        // backgroundColor: '#ed829f',
-        backgroundColor: '#0000ff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginLeft: 12,
-        margin: 30
-    },
+  buttonDefault: {
+    width: "90%",
+    paddingTop: 14,
+    paddingBottom: 14,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 12,
+    margin: 30,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    textAlign: "center",
+  },
+});
 
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
-        textAlign: 'center'
-    },
-})
-
-export default styles
+export default styles;
